5b190f133b3c4312b716bc32f172583791fbb7e6
Karl Macababbad <kmacababbad@yahoo.com.sg>
2025-08-19 17:44:20 +1200

    Removed missionx-api folder from tracking

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
 
-  // Function to fetch Oceania countries
+  // Function to fetch Oceania countries - API call to the backend
   const fetchOceaniaCountries = async () => {
     try {
       const response = await axios.get(`${API_HOST}/oceania`);
@@ -34,6 +34,10 @@ const App = () => {
         }
       } catch (error) {
         console.log('Error displaying Oceania countries:', error);
+        setCountries([{
+          Name: 'Unknown',
+          LifeExpectancy: 'Unknown'
+        }]);
         setError('Failed to display Oceania countries.');
       }
     };
